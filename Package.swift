563a8905950645e5a51a28d8db3b49a50c10ddfe
Thomas Rademaker <thomas@sparrowtek.com>
2023-10-15 23:41:10 -0400

    swift package created

--- Package.swift
+++ Package.swift
@@ -1,20 +1,20 @@
 // swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "LittleManComputerCLI",
+    platforms: [.macOS(.v14)],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
+        .package(url: "https://github.com/SparrowTek/CoreLittleManComputer.git", branch: "main"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "LittleManComputerCLI",
+            name: "LittleManComputer",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                "CoreLittleManComputer",
             ]
         ),
     ]
