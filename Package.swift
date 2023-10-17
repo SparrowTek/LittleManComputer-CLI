7583136e35a54e7022fcea88e682e53c490c33f3
Thomas Rademaker <thomas@sparrowtek.com>
2023-10-17 18:05:38 -0400

    WIP getting CLI working

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     targets: [
         .executableTarget(
-            name: "LittleManComputer",
+            name: "LMC",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
                 "CoreLittleManComputer",
